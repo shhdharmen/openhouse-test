1769b110b88ddb4914b6ad399d5d6879a77f3c6b
Dharmen Shah <dharmen.s@milestoneinternet.com>
2021-02-03 16:56:36 +0530

    add more tests

--- main.spec.js
+++ main.spec.js
@@ -23,3 +23,11 @@ test('"abc42" should become 0', () => {
 test('"-42" should become -42', () => {
   expect(stringToNumber("-42")).toBe(-42);
 });
+
+test('"Infinity" should become Infinity', () => {
+  expect(stringToNumber("Infinity")).toBe(Infinity);
+});
+
+test('"-Infinity" should become -Infinity', () => {
+  expect(stringToNumber("-Infinity")).toBe(-Infinity);
+});
